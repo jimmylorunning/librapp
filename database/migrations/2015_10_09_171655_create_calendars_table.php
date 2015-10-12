29f98dbd7b64574014d7a7ad641d5140e569a03b
jimmylorunning <jimmylorunning@gmail.com>
2015-10-12 12:39:17 -0400

    Add Calendar model and migration

---
+++ database/migrations/2015_10_09_171655_create_calendars_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCalendarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('calendars', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamp('starts');
+            $table->timestamp('ends');
+            $table->enum('audience', ['children', 'adult']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('calendars');
+    }
+}
