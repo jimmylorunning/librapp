4b95f125a57db0d09e1b6635d8931cee93b425c3
jimmylorunning <jimmylorunning@gmail.com>
2015-10-13 10:51:59 -0400

    Finish basic New Calendar form with the Form and HTML Builder package

--- resources/views/calendars/create.blade.php
+++ resources/views/calendars/create.blade.php
@@ -5,5 +5,25 @@
 @stop
 
 @section('content')
-Creation form goes here.
+  {!! Form::model($calendar = new \App\Calendar, ['url' => 'calendars']) !!}
+  <div class="form-group">
+    {!! Form::label('name', 'Name:' ) !!}
+    {!! Form::text('name', null, ['class' => 'form-control']) !!}
+  </div>
+  <div class="form-group">
+    {!! Form::label('begins', 'Start date:' ) !!}
+    {!! Form::input('date', 'begins', null, ['class' => 'form-control']) !!}
+  </div>
+  <div class="form-group">
+    {!! Form::label('ends', 'End date:' ) !!}
+    {!! Form::input('date', 'ends', null, ['class' => 'form-control']) !!}
+  </div>
+  <div class="form-group">
+    {!! Form::label('audience', 'Audience:' ) !!}
+    {!! Form::select('audience[]', ['adults' => 'adults', 'children' => 'children'], null, ['class' => 'form-control']) !!}
+  </div>
+  <div class="form-group">
+    {!! Form::submit('Add Calendar', ['class' => 'btn btn-primary']) !!}  
+  </div>
+  {!! Form::close() !!}
 @stop
