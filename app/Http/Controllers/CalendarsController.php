49e7cc652b6fac12d17388bfda73603e2d70f3d3
jimmylorunning <jimmylorunning@gmail.com>
2015-10-27 12:59:09 -0400

    Restrict calendar and events pages to logged in users only

--- app/Http/Controllers/CalendarsController.php
+++ app/Http/Controllers/CalendarsController.php
@@ -9,6 +9,12 @@ use App\Calendar;
 
 class CalendarsController extends Controller
 {
+  
+    public function __construct() 
+    {
+      $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -9,6 +9,12 @@ use App\Event;
 
 class EventsController extends Controller
 {
+
+    public function __construct() 
+    {
+      $this->middleware('auth');
+    }
+    
     /**
      * Display a listing of the resource.
      *
