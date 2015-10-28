8454e0c7730af2cb638d6d085e980d9b893ceaf0
jimmylorunning <jimmylorunning@gmail.com>
2015-10-28 10:23:41 -0400

    Add roles migration and model

--- app/User.php
+++ app/User.php
@@ -36,4 +36,32 @@ class User extends Model implements AuthenticatableContract,
      * @var array
      */
     protected $hidden = ['password', 'remember_token'];
+
+    public function roles()
+    {
+      return $this->belongsToMany('App\Role');
+    }
+
+    public function hasRole($name)
+    {
+      foreach($this->roles as $role)
+      {
+        if ($role->name == $name)
+        {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    public function assignRole($role)
+    {
+      return $this->roles()->attach($role);
+    }
+
+    public function removeRole($role)
+    {
+      return $this->roles()->detach($role);
+    }
+
 }
